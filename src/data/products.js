ccccf24c47947e4b5b56b8bba6133822c8fcc01a
Sergio Rodriguez <sergio159go@gmail.com>
2025-12-11 01:49:37 -0500

    arreglando fotos

--- src/data/products.js
+++ src/data/products.js
@@ -4,21 +4,21 @@ export const products = [
     title: "Marquina_Black",
     category: "Porcelanatos",
     price: 45.99,
-    image: "assets/Marquina_Black.jpeg"
+    image: "./img/Marquina_Black.jpeg"
   },
   {
     id: 2,
     title: "Portoro_Black",
     category: "Mosaicos",
     price: 12.50,
-    image: "assets/Portoro_Black.jpeg"
+    image: "./img/Portoro_Black.JPEG"
   },
   {
     id: 3,
     title: "Lavabo Mármol Blanco",
     category: "Baño",
     price: 180.00,
-    image: "https://images.unsplash.com/photo-1585058057285-d6023258c0c1?w=500&q=80"
+    image: "https://images.unsplash.com/photo-1594633313593-bab3825d0caf?w=500&q=80"
   },
   {
     id: 4,
